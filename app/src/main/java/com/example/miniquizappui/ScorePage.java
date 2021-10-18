ce05b2a2bb69932fc3b22ef066a9f6b35be53430
github <pragya.sharma2_cs19@gla.ac.in>
2021-10-18 21:19:32 +0530

    Code for score page completed.

--- app/src/main/java/com/example/miniquizappui/ScorePage.java
+++ app/src/main/java/com/example/miniquizappui/ScorePage.java
@@ -2,13 +2,29 @@ package com.example.miniquizappui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ScorePage extends AppCompatActivity {
-
+    Button b1;
+    TextView t1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score_page);
+        t1 = findViewById(R.id.textView4);
+        b1 = findViewById(R.id.button4);
+        t1.setText("Your Score : " + MainActivity.score);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(ScorePage.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
