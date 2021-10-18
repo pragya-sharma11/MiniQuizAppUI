46ce87b911fb277a2e6967525dc56ad321822ee3
github <pragya.sharma2_cs19@gla.ac.in>
2021-10-18 22:52:52 +0530

    make textToSpeech for score page.

--- app/src/main/java/com/example/miniquizappui/ScorePage.java
+++ app/src/main/java/com/example/miniquizappui/ScorePage.java
@@ -4,13 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 public class ScorePage extends AppCompatActivity {
     Button b1;
     TextView t1;
+    TextToSpeech t;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,9 +22,17 @@ public class ScorePage extends AppCompatActivity {
         t1 = findViewById(R.id.textView4);
         b1 = findViewById(R.id.button4);
         t1.setText("Your Score : " + MainActivity.score);
+        t = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                t.setLanguage(Locale.ENGLISH);
+                t.setSpeechRate(1.0f);
+            }
+        });
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                t.speak("Ready to retake the quiz..Here we go!!", TextToSpeech.QUEUE_FLUSH,null);
                 Intent i = new Intent(ScorePage.this, MainActivity.class);
                 startActivity(i);
                 finish();
